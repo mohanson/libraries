a619f9657b89791d775bf1503d3edcc28791081b
mohanson <mohanson@outlook.com>
2025-12-10 21:12:39 +0800

    2025-12-10 21:12:39

--- go/balloc/balloc.go
+++ go/balloc/balloc.go
@@ -19,12 +19,12 @@ type Block struct {
 
 // Buddy manages the buddy memory allocation.
 type Buddy struct {
-	freeList []*list.List
-	maxOrder int
-	maxTotal int
-	minBlock int
-	preAlloc []byte
-	syncLock *sync.Mutex
+	FreeList []*list.List
+	MaxOrder int
+	MaxTotal int
+	MinBlock int
+	PreAlloc []byte
+	SyncLock *sync.Mutex
 }
 
 // Calloc allocates a block of the requested size and ensure it's initialized to zero.
@@ -38,46 +38,46 @@ func (b *Buddy) Calloc(size int) Block {
 
 // Balloc allocates a block of the requested size.
 func (b *Buddy) Malloc(size int) Block {
-	b.syncLock.Lock()
-	defer b.syncLock.Unlock()
+	b.SyncLock.Lock()
+	defer b.SyncLock.Unlock()
 	doa.Doa(size > 0)
-	orderReal := log2(b.minBlock, max(clp2(size), b.minBlock))
+	orderReal := log2(b.MinBlock, max(clp2(size), b.MinBlock))
 	orderTemp := orderReal
-	for orderTemp <= b.maxOrder && b.freeList[orderTemp].Len() == 0 {
+	for orderTemp <= b.MaxOrder && b.FreeList[orderTemp].Len() == 0 {
 		orderTemp++
 	}
-	doa.Doa(orderTemp <= b.maxOrder)
-	blockElem := b.freeList[orderTemp].Front()
-	b.freeList[orderTemp].Remove(blockElem)
+	doa.Doa(orderTemp <= b.MaxOrder)
+	blockElem := b.FreeList[orderTemp].Front()
+	b.FreeList[orderTemp].Remove(blockElem)
 	block := blockElem.Value.(Block)
 	for orderTemp > orderReal {
 		orderTemp--
-		blockSize := b.minBlock << orderTemp
-		b.freeList[orderTemp].PushFront(Block{
+		blockSize := b.MinBlock << orderTemp
+		b.FreeList[orderTemp].PushFront(Block{
 			Index: block.Index + blockSize,
 			Order: orderTemp,
 		})
 		block.Order = orderTemp
 	}
-	block.Space = b.preAlloc[block.Index : block.Index+size]
+	block.Space = b.PreAlloc[block.Index : block.Index+size]
 	return block
 }
 
 // Free deallocates a block and merges with its buddy if possible.
 func (b *Buddy) Free(block Block) {
-	b.syncLock.Lock()
-	defer b.syncLock.Unlock()
+	b.SyncLock.Lock()
+	defer b.SyncLock.Unlock()
 	doa.Doa(block.Index >= 0)
-	doa.Doa(block.Index < b.maxTotal)
+	doa.Doa(block.Index < b.MaxTotal)
 	doa.Doa(block.Order >= 0)
-	doa.Doa(block.Order < b.maxOrder)
+	doa.Doa(block.Order < b.MaxOrder)
 	index := block.Index
 	order := block.Order
-	for order < b.maxOrder {
-		blockSize := b.minBlock << order
+	for order < b.MaxOrder {
+		blockSize := b.MinBlock << order
 		buddy := index ^ blockSize
 		buddyElem := (*list.Element)(nil)
-		for e := b.freeList[order].Front(); e != nil; e = e.Next() {
+		for e := b.FreeList[order].Front(); e != nil; e = e.Next() {
 			if b := e.Value.(Block); b.Index == buddy {
 				buddyElem = e
 				break
@@ -86,20 +86,20 @@ func (b *Buddy) Free(block Block) {
 		if buddyElem == nil {
 			break
 		}
-		b.freeList[order].Remove(buddyElem)
+		b.FreeList[order].Remove(buddyElem)
 		index = min(index, buddy)
 		order++
 	}
-	b.freeList[order].PushFront(Block{Index: index, Order: order})
+	b.FreeList[order].PushFront(Block{Index: index, Order: order})
 }
 
 // Get the size of idle memory.
 func (b *Buddy) Idle() int {
-	b.syncLock.Lock()
-	defer b.syncLock.Unlock()
+	b.SyncLock.Lock()
+	defer b.SyncLock.Unlock()
 	s := 0
-	for i, e := range b.freeList {
-		b := b.minBlock << i
+	for i, e := range b.FreeList {
+		b := b.MinBlock << i
 		s += e.Len() * b
 	}
 	return s
@@ -112,17 +112,17 @@ func New(maxTotal int, minBlock int) *Buddy {
 	doa.Doa(maxTotal > minBlock)
 	order := log2(minBlock, maxTotal)
 	buddy := &Buddy{
-		freeList: make([]*list.List, order+1),
-		maxOrder: order,
-		maxTotal: maxTotal,
-		minBlock: minBlock,
-		preAlloc: make([]byte, maxTotal),
-		syncLock: &sync.Mutex{},
+		FreeList: make([]*list.List, order+1),
+		MaxOrder: order,
+		MaxTotal: maxTotal,
+		MinBlock: minBlock,
+		PreAlloc: make([]byte, maxTotal),
+		SyncLock: &sync.Mutex{},
 	}
-	for i := range buddy.freeList {
-		buddy.freeList[i] = list.New()
+	for i := range buddy.FreeList {
+		buddy.FreeList[i] = list.New()
 	}
-	buddy.freeList[order].PushFront(Block{Index: 0, Order: order})
+	buddy.FreeList[order].PushFront(Block{Index: 0, Order: order})
 	return buddy
 }
 
