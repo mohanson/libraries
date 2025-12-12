746dfb98c1bb6b08d934d0c1096533794de85d10
mohanson <mohanson@outlook.com>
2025-12-12 16:49:45 +0800

    2025-12-12 16:49:45

---
+++ rust/balloc/examples/fuzz.rs
@@ -0,0 +1,50 @@
+use balloc::{Allocator, FREE_LIST, MAX_ORDER};
+use std::alloc::{GlobalAlloc, Layout};
+
+static mut RANDSEED: u64 = 0;
+pub unsafe fn rand() -> u64 {
+    unsafe {
+        RANDSEED = RANDSEED.wrapping_mul(6364136223846793005).wrapping_add(1);
+        RANDSEED
+    }
+}
+
+pub struct Knowledge {
+    pub ptr: *mut u8,
+    pub layout: Layout,
+}
+
+fn main() {
+    unsafe {
+        let allocator = Allocator::global();
+        let mut vk: Vec<Knowledge> = vec![];
+        println!("main: fuzz");
+        for _ in 0..1024 * 1024 {
+            let action_random = rand() as usize % 1024;
+            let action = if vk.len() > action_random { 1 } else { 0 };
+            match action {
+                0 => {
+                    let layout = Layout::from_size_align(rand() as usize % 4096, 1).unwrap();
+                    let ptr = allocator.alloc(layout);
+                    vk.push(Knowledge { ptr, layout });
+                }
+                1 => {
+                    let idx = rand() as usize % vk.len();
+                    let knowledge = vk.swap_remove(idx);
+                    allocator.dealloc(knowledge.ptr, knowledge.layout);
+                }
+                _ => unreachable!(),
+            }
+        }
+        println!("main: fuzz free {} knowledge", vk.len());
+        for knowledge in vk.drain(..) {
+            allocator.dealloc(knowledge.ptr, knowledge.layout);
+        }
+        assert_eq!(vk.len(), 0);
+        for i in 0..MAX_ORDER {
+            assert_eq!(FREE_LIST[i], usize::MAX);
+        }
+        assert_eq!(FREE_LIST[MAX_ORDER], 0);
+        println!("main: fuzz done");
+    }
+}
---
+++ rust/balloc/src/lib.rs
@@ -0,0 +1,218 @@
+//! A hand written buddy allocator implementation for no_std environments.
+//!
+//! This module implements a buddy memory allocator that manages a fixed-size memory pool.
+//! The buddy allocator works by recursively splitting memory blocks into pairs of equal-sized "buddies" until it finds
+//! a block of the appropriate size. When memory is freed, it attempts to merge adjacent buddy blocks back together to
+//! reduce fragmentation.
+
+use core::alloc::{GlobalAlloc, Layout};
+use core::cell::UnsafeCell;
+use core::cmp::min;
+use core::ptr::addr_of_mut;
+
+pub const MIN_BLOCK: usize = 64;
+pub const MAX_ORDER: usize = 14;
+pub const MAX_TOTAL: usize = 1024 * 1024;
+pub const PTR_ALLOC: *mut u8 = addr_of_mut!(PRE_ALLOC) as *mut u8;
+pub static mut FREE_LIST: [usize; MAX_ORDER + 1] = {
+    let mut list = [usize::MAX; MAX_ORDER + 1];
+    list[MAX_ORDER] = 0;
+    list
+};
+pub static mut PRE_ALLOC: [u8; MAX_TOTAL] = {
+    assert!(matches!(usize::BITS, 32 | 64));
+    let mut alloc = [0; MAX_TOTAL];
+    if usize::BITS >= 32 {
+        alloc[0] = 0xff;
+        alloc[1] = 0xff;
+        alloc[2] = 0xff;
+        alloc[3] = 0xff;
+    }
+    if usize::BITS >= 64 {
+        alloc[4] = 0xff;
+        alloc[5] = 0xff;
+        alloc[6] = 0xff;
+        alloc[7] = 0xff;
+    }
+    alloc
+};
+
+/// Buddy allocation algorithm implementation.
+pub struct Algorithm;
+/// Information about allocated memory blocks.
+#[derive(Clone, Copy)]
+pub struct Blockinfo {
+    pub offset: usize,
+    pub length: usize,
+}
+
+impl Algorithm {
+    pub fn alloc(&mut self, order: usize) -> Blockinfo {
+        unsafe {
+            if order > MAX_ORDER {
+                let block_offset = usize::MAX;
+                return Blockinfo {
+                    offset: block_offset,
+                    length: 0,
+                };
+            }
+            let block_size = MIN_BLOCK << order;
+            if FREE_LIST[order] != usize::MAX {
+                let block_offset = FREE_LIST[order];
+                let block_ptr = PTR_ALLOC.add(block_offset);
+                FREE_LIST[order] = uldr(block_ptr);
+                return Blockinfo {
+                    offset: block_offset,
+                    length: block_size,
+                };
+            }
+            let block = self.alloc(order + 1);
+            let block_offset = block.offset;
+            if block_offset == usize::MAX {
+                return Blockinfo {
+                    offset: block_offset,
+                    length: 0,
+                };
+            }
+            let buddy_offset = block_offset + block_size;
+            let buddy_ptr = PTR_ALLOC.add(buddy_offset);
+            ustr(buddy_ptr, usize::MAX);
+            FREE_LIST[order] = buddy_offset;
+            Blockinfo {
+                offset: block_offset,
+                length: block_size,
+            }
+        }
+    }
+
+    pub fn close(&mut self, block: Blockinfo) {
+        unsafe {
+            if block.offset == usize::MAX {
+                return;
+            }
+            let order = log2(MIN_BLOCK, block.length);
+            let block_idx = block.offset / block.length;
+            let buddy_idx = block_idx ^ 1;
+            let buddy_offset = buddy_idx * block.length;
+            let buddy = Blockinfo {
+                offset: buddy_offset,
+                length: block.length,
+            };
+            let upper = Blockinfo {
+                offset: min(block.offset, buddy_offset),
+                length: block.length << 1,
+            };
+            if self.state(buddy) == 0 {
+                self.merge(buddy);
+                self.close(upper);
+                return;
+            }
+            let block_ptr = PTR_ALLOC.add(block.offset);
+            ustr(block_ptr, FREE_LIST[order]);
+            FREE_LIST[order] = block.offset;
+        }
+    }
+
+    pub fn state(&mut self, buddy: Blockinfo) -> usize {
+        unsafe {
+            let order = log2(MIN_BLOCK, buddy.length);
+            let mut n = FREE_LIST[order];
+            let mut m: usize;
+            loop {
+                if n == usize::MAX {
+                    return 1;
+                }
+                m = uldr(PTR_ALLOC.add(n));
+                if n == buddy.offset {
+                    return 0;
+                }
+                n = m;
+            }
+        }
+    }
+
+    pub fn merge(&mut self, buddy: Blockinfo) {
+        unsafe {
+            let order = log2(MIN_BLOCK, buddy.length);
+            let mut n = FREE_LIST[order];
+            let mut m: usize;
+            loop {
+                if n == usize::MAX {
+                    break;
+                }
+                m = uldr(PTR_ALLOC.add(n));
+                if n == buddy.offset {
+                    FREE_LIST[order] = m;
+                    return;
+                }
+                if m == buddy.offset {
+                    ustr(PTR_ALLOC.add(n), uldr(PTR_ALLOC.add(m)));
+                    break;
+                }
+                n = m;
+            }
+        }
+    }
+}
+
+pub struct Allocator {
+    inner: UnsafeCell<Algorithm>,
+}
+
+impl Allocator {
+    pub const fn global() -> Self {
+        Allocator {
+            inner: UnsafeCell::new(Algorithm {}),
+        }
+    }
+}
+
+unsafe impl GlobalAlloc for Allocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        unsafe {
+            let inner = &mut *self.inner.get();
+            let order = log2(MIN_BLOCK, clp2(layout.size()).max(MIN_BLOCK));
+            let block = inner.alloc(order);
+            PTR_ALLOC.add(block.offset)
+        }
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        unsafe {
+            let inner = &mut *self.inner.get();
+            let order = log2(MIN_BLOCK, clp2(layout.size()).max(MIN_BLOCK));
+            let block = Blockinfo {
+                offset: ptr.offset_from(PTR_ALLOC) as usize,
+                length: MIN_BLOCK << order,
+            };
+            inner.close(block);
+        }
+    }
+}
+
+unsafe impl Sync for Allocator {}
+
+fn clp2(n: usize) -> usize {
+    n.next_power_of_two()
+}
+
+fn log2(m: usize, n: usize) -> usize {
+    let mut m = m;
+    for i in 0..64 {
+        if m == n {
+            return i;
+        }
+        m <<= 1;
+    }
+    unreachable!()
+}
+
+fn uldr(p: *mut u8) -> usize {
+    unsafe { *(p as *const usize) }
+}
+
+fn ustr(p: *mut u8, n: usize) {
+    unsafe {
+        *(p as *mut usize) = n;
+    }
+}
