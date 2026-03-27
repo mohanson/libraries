7ec54f503f3f903d6c01e5b2667ad02bc77353f7
mohanson <mohanson@outlook.com>
2026-03-27 10:27:23 +0800

    2026-03-27 10:27:23

--- python/io.py
+++ python/io.py
@@ -1,7 +1,7 @@
-import typing
+import io
 
 
-def read_full(reader: typing.BinaryIO, n: int) -> bytearray:
+def read_full(reader: io.IOBase, n: int) -> bytearray:
     # Read exactly n bytes from reader, or raise EOFError.
     data = bytearray()
     while len(data) < n:
