8abe4b5e8da642b44bcac1070603a4b517d19633
mohanson <mohanson@outlook.com>
2026-05-20 10:04:11 +0800

    2026-05-20 10:04:11

---
+++ rust/rand/src/lib.rs
@@ -0,0 +1,38 @@
+//! A simple Linear Congruential Generator (LCG) implementation for filling a byte slice with pseudo-random data. Same
+//! as the one used by POSIX drand48.
+//! See: https://www.man7.org/linux/man-pages/man3/lcong48.3.html
+use core::sync::atomic::{AtomicU64, Ordering};
+
+const LCG_A: u64 = 0x5DEECE66D;
+const LCG_C: u64 = 0xB;
+const LCG_M: u64 = 1u64 << 48;
+static SEED: AtomicU64 = AtomicU64::new(0x12345678);
+
+pub fn u8() -> u8 {
+    u48() as u8
+}
+
+pub fn u16() -> u16 {
+    u48() as u16
+}
+
+pub fn u32() -> u32 {
+    u48() as u32
+}
+
+pub fn u48() -> u64 {
+    let mut old = SEED.load(Ordering::Relaxed);
+    loop {
+        let new = (LCG_A.wrapping_mul(old).wrapping_add(LCG_C)) % LCG_M;
+        match SEED.compare_exchange_weak(old, new, Ordering::Relaxed, Ordering::Relaxed) {
+            Ok(_) => return new,
+            Err(v) => old = v,
+        }
+    }
+}
+
+pub fn u64() -> u64 {
+    let hi = u48() << 16;
+    let lo = u48();
+    hi | lo
+}
