57f342502b927d5695f85a06a54f593c6192b38d
mohanson <mohanson@outlook.com>
2026-07-06 18:36:22 +0800

    2026-07-06 18:36:22

--- go/getpass/arch_darwin.go
+++ go/getpass/arch_darwin.go
@@ -2,11 +2,32 @@ package getpass
 
 import (
 	"fmt"
+	"os"
+	"syscall"
+	"unsafe"
 )
+
+func sttyEchoNo() {
+	termios := syscall.Termios{}
+	fd := os.Stdout.Fd()
+	syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(syscall.TIOCGETA), uintptr(unsafe.Pointer(&termios)))
+	termios.Lflag &^= syscall.ECHO
+	syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(syscall.TIOCSETA), uintptr(unsafe.Pointer(&termios)))
+}
+
+func sttyEchoOn() {
+	termios := syscall.Termios{}
+	fd := os.Stdout.Fd()
+	syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(syscall.TIOCGETA), uintptr(unsafe.Pointer(&termios)))
+	termios.Lflag |= syscall.ECHO
+	syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(syscall.TIOCSETA), uintptr(unsafe.Pointer(&termios)))
+}
 
 func GetPass(prompt string) string {
 	pass := ""
 	fmt.Print(prompt)
+	sttyEchoNo()
 	fmt.Scanln(&pass)
+	sttyEchoOn()
 	return pass
 }
--- go/getpass/arch_windows.go
+++ go/getpass/arch_windows.go
@@ -2,11 +2,38 @@ package getpass
 
 import (
 	"fmt"
+	"os"
+	"syscall"
+	"unsafe"
 )
+
+var (
+	kernel32           = syscall.NewLazyDLL("kernel32.dll")
+	procGetConsoleMode = kernel32.NewProc("GetConsoleMode")
+	procSetConsoleMode = kernel32.NewProc("SetConsoleMode")
+)
+
+func sttyEchoNo() {
+	handle := os.Stdin.Fd()
+	mode := uint32(0)
+	procGetConsoleMode.Call(handle, uintptr(unsafe.Pointer(&mode)))
+	mode &= 0xfffffffb
+	procSetConsoleMode.Call(handle, uintptr(mode))
+}
+
+func sttyEchoOn() {
+	handle := os.Stdin.Fd()
+	mode := uint32(0)
+	procGetConsoleMode.Call(handle, uintptr(unsafe.Pointer(&mode)))
+	mode |= 0x00000004
+	procSetConsoleMode.Call(handle, uintptr(mode))
+}
 
 func GetPass(prompt string) string {
 	pass := ""
 	fmt.Print(prompt)
+	sttyEchoNo()
 	fmt.Scanln(&pass)
+	sttyEchoOn()
 	return pass
 }
